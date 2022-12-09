13b88086d5c737e1a9cb62ad648d44787ebc43f9
Sebastien Guillemot <sebastiengllmt@gmail.com>
2022-12-10 04:25:30 +0900

    Fix type definitions

--- src/registryTypes.ts
+++ src/registryTypes.ts
@@ -1,11 +1,13 @@
 export interface Registry {
-    Mainnet: Net;
-    Testnet: Net;
+  Mainnet: Net;
+  LegacyTestnet: Net;
+  PreProduction: Net;
+  Preview: Net;
 }
 
 export interface Net {
-    Name:         string;
-    NetworkId:    number;
-    NetworkMagic: number;
-    GenesisHash:  string;
+  Name: string;
+  NetworkId: number;
+  NetworkMagic: number;
+  GenesisHash: string;
 }
